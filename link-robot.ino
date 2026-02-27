137ddb524e1d70cd3b1592acc1b28f9b85d29927
happyriku <lijiushiqiao@gmail.com>
2026-02-27 20:10:20 +0900

    delete: コメントアウトしてあったプログラム

--- link-robot.ino
+++ link-robot.ino
@@ -12,22 +12,3 @@ void loop() {
   robot.drawString("O");
   delay(5000);
 }
-
-// #include <Servo.h>
-
-// Servo servoL;
-// Servo servoR;
-
-// const int SERVO_L_PIN = 13;
-// const int SERVO_R_PIN = 10;
-
-// void setup() {
-//   pinMode(LED_BUILTIN, OUTPUT);
-// }
-
-// void loop() {
-//   digitalWrite(LED_BUILTIN, HIGH);
-//   delay(500);
-//   digitalWrite(LED_BUILTIN, LOW);
-//   delay(500);
-// }
