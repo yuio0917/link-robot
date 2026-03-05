26fcf081aace86a515314b6db8046e4d35462fb2
happyriku <lijiushiqiao@gmail.com>
2026-03-05 16:34:02 +0900

    feature: Serial Communication

--- include/LinkRobot.hpp
+++ include/LinkRobot.hpp
@@ -6,6 +6,7 @@
 #include <vector>  
 #include <iostream>
 #include <cmath>
+#include <string>
 
 class LinkRobot {
     private:
--- link-robot.ino
+++ link-robot.ino
@@ -9,6 +9,12 @@ void setup(){
 
 void loop() {
   robot.InitPosition(10, 180);
-  robot.drawString("X");
+  if (Serial.available()){
+    Serial.println("文字列を入力してください．");
+    String arduinoStr = Serial.readString(); //改行含む
+    arduinoStr.trim(); //改行や空白を除去
+    std::string str = arduinoStr.c_str(); //arduinoのString型をstd::string型に変更．
+    robot.drawString(str);  
+  }
   delay(5000);
 }
